8d4114665d480949c3cef1907bdfb4bf25cd602d
jaecheon12 <hijde@tvcf.co.kr>
2023-10-06 09:43:50 +0900

    0724-find-pivot-index

---
+++ jaecheon/0724-find-pivot-index/0724-find-pivot-index.py
@@ -0,0 +1,18 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        length = len(nums)
+        leftSum = 0
+        rightSum = sum(nums[1:])
+
+        if leftSum == rightSum:
+                return 0
+
+        for pos in range(1,length):
+            leftSum += nums[pos-1]
+            rightSum -= nums[pos]
+            
+            if leftSum == rightSum:
+                return pos
+
+        return -1            
+    
